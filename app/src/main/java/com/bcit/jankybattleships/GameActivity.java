21c8269ded7648dc61835965baaa59f839a5c5e6
migu55 <miguelmarasigan55@gmail.com>
2022-03-18 18:02:48 -0700

    Initial Layouts and Building Blocks

--- app/src/main/java/com/bcit/jankybattleships/GameActivity.java
+++ app/src/main/java/com/bcit/jankybattleships/GameActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
     }
 }
-
-//frame layout- overlay login circle?
