af8f13c904e32e27b2f263018b2bce1f62719b28
Miguel Marasigan <miguelmarasigan55@gmail.com>
2022-03-21 15:42:30 -0700

    Modified SDK and testing firebase login

--- app/src/main/java/com/bcit/jankybattleships/MainActivity.java
+++ app/src/main/java/com/bcit/jankybattleships/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
@@ -33,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createSignInIntent();
+            }
+        });
     }
 
     //onClick for login
