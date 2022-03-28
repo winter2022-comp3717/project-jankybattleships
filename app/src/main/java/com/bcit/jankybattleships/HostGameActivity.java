155b023ceff3bdcb60bafeb8495d894e03f0313a
Bach Le <bachcyp@gmail.com>
2022-03-27 21:18:26 -0700

    fixed game code validation

--- app/src/main/java/com/bcit/jankybattleships/HostGameActivity.java
+++ app/src/main/java/com/bcit/jankybattleships/HostGameActivity.java
@@ -1,5 +1,6 @@
 package com.bcit.jankybattleships;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -12,13 +13,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
 public class HostGameActivity extends AppCompatActivity {
 
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private static final int REFRESH_DELAY = 5000;
     private final GameSession session = new GameSession();
 
@@ -47,13 +54,26 @@ public class HostGameActivity extends AppCompatActivity {
     private void validateRoomCode() {
         TextView roomCodeTextView = findViewById(R.id.textView_host_roomcode);
         TextView errorTextView = findViewById(R.id.textView_host_error);
-        if (session.isUsedRoomCode(roomCodeTextView.getText().toString())) {
-            errorTextView.setTextColor(getColor(R.color.error_red));
-            errorTextView.setText(R.string.used_room_code_warning);
-        } else {
-            errorTextView.setTextColor(getColor(R.color.black));
-            errorTextView.setText(R.string.valid_room_code);
-        }
+        DocumentReference docRef = db.collection("sessions").document(roomCodeTextView.getText().toString());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        errorTextView.setTextColor(getColor(R.color.error_red));
+                        errorTextView.setText(R.string.used_room_code_warning);
+                        Log.d("INFO", "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        errorTextView.setTextColor(getColor(R.color.black));
+                        errorTextView.setText(R.string.valid_room_code);
+                        Log.d("INFO", "No such document");
+                    }
+                } else {
+                    Log.d("INFO", "get failed with ", task.getException());
+                }
+            }
+        });
     }
 
     private void startWaitForP2() {
