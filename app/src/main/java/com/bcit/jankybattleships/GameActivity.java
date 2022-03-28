8f5e17bf4c1819241b4bbc1d8ccd9e0a1faaaa3a
Jason Banh <jbanh2@gmail.com>
2022-03-27 19:14:01 -0700

    Got the game setup layout looking... OK?

--- app/src/main/java/com/bcit/jankybattleships/GameActivity.java
+++ app/src/main/java/com/bcit/jankybattleships/GameActivity.java
@@ -3,6 +3,8 @@ package com.bcit.jankybattleships;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_setup);
+    }
+
+    /**
+     * Display the given grid to the fragment.
+     */
+    public void displayGrid(View view) {
+    }
+
+    /**
+     * Swap fragment view between your grid and your opponents.
+     */
+    public void swapView(View view) {
+    }
+
+    /**
+     * Set the button color according to presence of a ship.
+     * @param view:
+     */
+    public void targetEffect(View view) {
     }
 }
---
+++ app/src/main/java/com/bcit/jankybattleships/GameSetup.java
@@ -0,0 +1,18 @@
+package com.bcit.jankybattleships;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_setup2);
+    }
+
+    /**
+     *
+     */
+}
