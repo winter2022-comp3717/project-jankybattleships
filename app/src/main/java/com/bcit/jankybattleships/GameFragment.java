a914d722be12d61cad90db8a03328c5d421eadf6
Jason Banh <jbanh2@gmail.com>
2022-03-27 23:33:29 -0700

    Partial work done to take coordinate strings and parse them.

--- app/src/main/java/com/bcit/jankybattleships/GameFragment.java
+++ app/src/main/java/com/bcit/jankybattleships/GameFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +62,5 @@ public class GameFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false);
     }
-
 
 }
--- app/src/main/java/com/bcit/jankybattleships/GameSetup.java
+++ app/src/main/java/com/bcit/jankybattleships/GameSetup.java
@@ -3,6 +3,10 @@ package com.bcit.jankybattleships;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.sql.Array;
 
 public class GameSetup extends AppCompatActivity {
 
@@ -10,6 +14,19 @@ public class GameSetup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_setup2);
+
+        Button submitPlacement = findViewById(R.id.confirm_placement);
+        EditText patrolPosition = findViewById(R.id.patrol_pos_input);
+        EditText destroyPosition = findViewById(R.id.destroyer_pos_input);
+        EditText cruisePosition = findViewById(R.id.cruiser_pos_input);
+
+        String patrolPositionString = patrolPosition.getText().toString();
+        String destroyPositionString = destroyPosition.getText().toString();
+        String cruisePositionString = cruisePosition.getText().toString();
+
+
+
+        submitPlacement.setOnClickListener(view -> submitPositions());
     }
 
     /**
@@ -19,5 +36,13 @@ public class GameSetup extends AppCompatActivity {
         /*
         Take the inputs from the text boxes, convert them to parts a grid and submit to Firebase.
          */
+    }
+
+    /**
+     * Helper to convert string coordinates into 2D array co-ordinates.
+     * @param input a String in format (X,X X,X)
+     */
+    public String[] placementHelper(String input) {
+        return input.split(" ");
     }
 }
